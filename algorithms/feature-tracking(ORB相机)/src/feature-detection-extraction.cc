43a646aadfbfedb1948c93bc1b1f2d0a8d14ad55
lovelyyoshino <mpl9725440@gmail.com>
2023-01-16 18:41:47 +0800

    orb

--- algorithms/feature-tracking(ORB相机)/src/feature-detection-extraction.cc
+++ algorithms/feature-tracking(ORB相机)/src/feature-detection-extraction.cc
@@ -52,20 +52,21 @@ void FeatureDetectorExtractor::initialize() {
       detector_settings_.orb_detector_WTA_K,
       detector_settings_.orb_detector_score_type,
       detector_settings_.orb_detector_patch_size,
-      detector_settings_.orb_detector_fast_threshold);
+      detector_settings_.orb_detector_fast_threshold);  // 通过Opencv创建ORB
 
   switch (extractor_settings_.descriptor_type) {
     case FeatureTrackingExtractorSettings::DescriptorType::kBrisk:
       extractor_ = new brisk::BriskDescriptorExtractor(
           extractor_settings_.rotation_invariant,
-          extractor_settings_.scale_invariant);
+          extractor_settings_.scale_invariant);  // 通过Brisk创建描述子
       break;
     case FeatureTrackingExtractorSettings::DescriptorType::kOcvFreak:
       extractor_ = cv::xfeatures2d::FREAK::create(
           extractor_settings_.rotation_invariant,
           extractor_settings_.scale_invariant,
           extractor_settings_.freak_pattern_scale,
-          detector_settings_.orb_detector_pyramid_levels);
+          detector_settings_
+              .orb_detector_pyramid_levels);  // 通过Freak创建描述子
       break;
     default:
       LOG(FATAL) << "Unknown descriptor type.";
@@ -73,24 +74,27 @@ void FeatureDetectorExtractor::initialize() {
   }
 }
 
-cv::Ptr<cv::DescriptorExtractor> FeatureDetectorExtractor::getExtractorPtr()
+cv::Ptr<cv::DescriptorExtractor>
+FeatureDetectorExtractor::getExtractorPtr()  // 获取描述子
     const {
   return extractor_;
 }
 
+// 检测特征点
 void FeatureDetectorExtractor::detectAndExtractFeatures(
     aslam::VisualFrame* frame) const {
-  CHECK_NOTNULL(frame);
-  CHECK(frame->hasRawImage())
-      << "Can only detect keypoints if the frame has a raw image";
+  CHECK_NOTNULL(frame);  // 检测特征点的帧不能为空
+  CHECK(frame->hasRawImage()) << "Can only detect keypoints if the frame has a "
+                                 "raw image";  // 检测特征点的帧必须有原始图像
   CHECK_EQ(
       camera_.getId(),
-      CHECK_NOTNULL(frame->getCameraGeometry().get())->getId());
+      CHECK_NOTNULL(frame->getCameraGeometry().get())
+          ->getId());  // 检测特征点的帧的相机必须和当前相机一致
 
-  timing::Timer timer_detection("keypoint detection");
+  timing::Timer timer_detection("keypoint detection");  // 计时器
 
   std::vector<cv::KeyPoint> keypoints_cv;
-  const cv::Mat& image = frame->getRawImage();
+  const cv::Mat& image = frame->getRawImage();  // 获取原始图像
 
   if (detector_settings_.gridded_detector_use_gridded) {
     // gridded detection to ensure a certain distribution of keypoints across
@@ -106,9 +110,9 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
         detector_settings_.gridded_detector_num_grid_cols,
         detector_settings_.gridded_detector_num_grid_rows,
         detector_settings_.gridded_detector_num_threads_per_image,
-        &keypoints_cv);
+        &keypoints_cv);  // 通过网格检测特征点
   } else {
-    detector_->detect(image, keypoints_cv);
+    detector_->detect(image, keypoints_cv);  // 通过ORB检测特征点
 
     if (detector_settings_.detector_use_nonmaxsuppression) {
       timing::Timer timer_nms("non-maximum suppression");
@@ -116,14 +120,15 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
           camera_.imageHeight(),
           detector_settings_.detector_nonmaxsuppression_radius,
           detector_settings_.detector_nonmaxsuppression_ratio_threshold,
-          &keypoints_cv);
+          &keypoints_cv);  // 通过非极大值抑制
 
       statistics::StatsCollector stat_nms(
-          "non-maximum suppression (1 image) in ms");
-      stat_nms.AddSample(timer_nms.Stop() * 1000);
+          "non-maximum suppression (1 image) in ms");  // 统计
+      stat_nms.AddSample(timer_nms.Stop() * 1000);     // 计时器
     }
     cv::KeyPointsFilter::retainBest(
-        keypoints_cv, detector_settings_.max_feature_count);
+        keypoints_cv,
+        detector_settings_.max_feature_count);  // 保留最好的特征点
   }
 
   // The ORB detector tries to always return a constant number of keypoints.
@@ -135,10 +140,11 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
   std::vector<cv::KeyPoint>::iterator it_erase_from = std::remove_if(
       keypoints_cv.begin(), keypoints_cv.end(), [this](const cv::KeyPoint& kp) {
         return kp.response <= detector_settings_.orb_detector_score_lower_bound;
-      });
+      });  // 移除低分数的特征点
   keypoints_cv.erase(it_erase_from, keypoints_cv.end());
 
-  num_removed_keypoints -= keypoints_cv.size();
+  num_removed_keypoints -=
+      keypoints_cv.size();  // 根据keypoints_cv所得到的移除的特征点数量
   VLOG(4) << "Number of removed low score keypoints: " << num_removed_keypoints;
 
   if (extractor_settings_.flip_descriptor) {
@@ -158,7 +164,7 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
     //   that rely on orientation information provided by some detectors
     //   (e.g. ORB detector).
     for (cv::KeyPoint& keypoint : keypoints_cv) {
-      keypoint.angle = -1.0f;
+      keypoint.angle = -1.0f;  // 将特征点的角度设为-1
     }
   }
   timer_detection.Stop();
@@ -168,7 +174,9 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
   // Compute the descriptors.
   cv::Mat descriptors_cv;
   if (!keypoints_cv.empty()) {
-    extractor_->compute(frame->getRawImage(), keypoints_cv, descriptors_cv);
+    extractor_->compute(
+        frame->getRawImage(), keypoints_cv,
+        descriptors_cv);  // 计算特征点的描述子
   } else {
     descriptors_cv = cv::Mat(0, 0, CV_8UC1);
   }
@@ -179,8 +187,8 @@ void FeatureDetectorExtractor::detectAndExtractFeatures(
   // keypoints as downstream code may rely on the keypoints being set.
   aslam::insertCvKeypointsAndDescriptorsIntoEmptyVisualFrame(
       keypoints_cv, descriptors_cv, detector_settings_.keypoint_uncertainty_px,
-      frame);
+      frame);  // 将特征点和描述子插入到视觉帧中
 
-  CHECK(frame->hasKeypointMeasurements());
+  CHECK(frame->hasKeypointMeasurements());  // 检查视觉帧是否有特征点测量
 }
 }  // namespace feature_tracking
